071e964969030bcc73ba00465d5c9946c63753d8
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-27 15:02:30 -0400

    Commit #1

---
+++ Assets/movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 move;
+    private Vector2 mousePos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input for movement
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        // Get mouse position in world coordinates
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        // Move the player
+        rb.MovePosition(rb.position + move.normalized * moveSpeed * Time.fixedDeltaTime);
+
+        // Rotate the player to face the mouse
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
